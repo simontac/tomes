c5ca2684fd09f712ce9fbdf9f72ee8c8d07434f3
simontac <lorenzo.simontacchi@gmail.com>
2025-11-16 19:54:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-document.querySelectorAll('.book-entry').forEach(entry => {
-  const main = entry.querySelector('.book-entry-main-info');
-  const extra = entry.querySelector('.book-entry-extra-info');
-
-  main.addEventListener('click', () => {
-    extra.classList.toggle('hidden');
-  });
-});
-
+// Showing the form to add new books
+// Transforming the "+" button to a "x" button
 document.querySelectorAll('.app-container').forEach(i => {
     const button = i.querySelector('.add-button');
     const popup = i.querySelector('.add-book-popup-form');
@@ -15,4 +8,21 @@ document.querySelectorAll('.app-container').forEach(i => {
         popup.classList.toggle('hidden');
         button.classList.toggle('rotated');
     });
+});
+
+// Changing toggle state when clicking on it
+const toggleButton = document.querySelector('.entries-sort svg');
+toggleButton.addEventListener('click', () => {
+  toggleButton.classList.toggle('toggled');
+});
+
+// Showing book entry extra info on click over main info area
+document.querySelector('.app-container').addEventListener('click', (event) => {
+  const main = event.target.closest('.book-entry-main-info');
+  if (!main) return;
+
+  const bookEntry = main.closest('.book-entry');
+  const extraInfo = bookEntry.querySelector('.book-entry-extra-info');
+
+  extraInfo.classList.toggle('hidden');
 });
